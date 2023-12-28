dea44ec5a600fccb639cfdcea99a6c5a5a1d5cdc
3DSA <dsusheelkar@ucsd.edu>
2023-12-27 21:38:22 -0800

    added index.html file with just boilerplate, added main.js with all the code for the rock, paper, scissors game. Added Readme for project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Basic Rock, Paper, Scissors game in javascript  
+use of functions to ask player choice and amount of rounds to be played  
+have fun!  
---
+++ main.js
@@ -0,0 +1,51 @@
+function getComputerChoice () {
+    const choice = Math.random()
+    if(choice < (1/3)) {
+        return "rock";
+    }
+    else if(choice < 2/3) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+function playRound(player, comp) {
+    if(player===comp) {
+        return "It's a draw!";
+    }
+    else if(player==="rock") {
+        if(comp==="paper") {
+            return "You lost!";
+        }
+        else { //scissors
+            return "You won!";
+        }
+    }
+    else if (player==="paper") {
+        if(comp==="rock") {
+            return "You won!";
+        }
+        else { //scissors
+            return "You lost!";
+        }
+    }
+    else { //scissors
+        if(comp==="paper") {
+            return "You won!";
+        }
+        else { //rock
+            return "You lost!";
+        }
+    }
+}
+function game(gamesplayed) {
+    for(let i = 0; i < gamesplayed; ++i) {
+        let compChoice = getComputerChoice();
+        let playerChoice = prompt("Choose rock, paper, or scissors!");
+        playerChoice = playerChoice.toLowerCase();
+        alert(playRound(playerChoice,compChoice)); 
+    }
+}
+rounds = prompt("How many rounds do you want to play!");
+game(rounds);
